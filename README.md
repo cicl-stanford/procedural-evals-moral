03e7882830172225f83ea85f53681b4e9e873cc0
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-09-09 10:23:34 -0700

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# procedural-evals-moral
+##  
+
+A Domain-Agnostic Method for Procedurally Generating LLM Evaluations
+
+![Causal Template -> Prompt Template -> Test Items](./assets/template.png)
+
+
+### 🧐 What is this?
+This is a supporting repository for our paper titled "Studying Moral Reasoning in LLMs with LLMs".
+We develop a method that uses large language models (LLMs) to procedurally generate evaluations for other LLMs. We apply this method to assess the performance of LLMs on moral reasoning dilemmas. 
+
+
+### 📂 Repo structure
+```
+├── code                 
+│   └── analysis
+│   └── prolific-exp-1
+│   └── prompt_instructions
+│   └── src 
+├── data   
+│   ├── morality
+│   └── prolific
+├── .gitignore
+├── LICENSE            
+└── requirements.txt
+```
+
+### 🚀 Getting started  
+##### Using miniforge
+1. install miniforge from `https://github.com/conda-forge/miniforge` (eg `Miniforge3-MacOSX-arm64`)
+2. `bash Miniforge3-MacOSX-arm64.sh`
+3. close terminal
+4. `conda create --name name-of-my-env python==3.10`
+5. `pip install -r requirements.txt` 
+
+#### Generating MoralEvals
+Prompt for generating MoralEvals is in `code/prompt_instructions/morality_stage_1.txt` and the python script is at `code/src/morality_stage_1.py`. To generate, run the following commands:
+1. `cd code/src`
+2. `python morality_stage_1.py`
+2. `python morality_stage_2.py`
+3. `python generate_conditions.py`
+
+#### Human Experiments
+We provide code to run Human experiments of 3 kinds:
+1. Expert Ratings: `code/src/expert_evaluate.py`
+2. Prolific Experiment for Testing Human Participants: `code/prolific-exp-1`
+
+#### Evaluating Models
+We provide code to evaluate models on BigToM in `code/src/evaluate_conditions.py`. More specific experiment scripts are available in `code/scripts`.
---
+++ src/morality_stage_1.py
@@ -0,0 +1,178 @@
+import random
+import csv
+import tqdm
+import argparse
+import ast
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from crfm import crfmChatLLM
+
+from utils import push_data, get_num_items, get_vars_from_out
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+           'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z']
+DATA_DIR = '../data'
+PROMPT_DIR = '../prompt_instructions'
+WORDS_DIR = '../words'
+REPO_URL = 'https://github.com/ayeshakhawaja/moral-judgment-prompt.git'
+CSV_NAME = 'morality_stage_1'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='openai/gpt-4-0314', help='model name')
+parser.add_argument('--temperature', type=float, default=0.5, help='temperature')
+parser.add_argument('--max_tokens', type=int, default=2000, help='max tokens')
+# change num completions to 10
+parser.add_argument('--num_completions', type=int, default=1, help='number of completions')
+parser.add_argument('--num_shots', type=int, default=3, help='number of shots')
+parser.add_argument('--num_stories', type=int, default=1, help='number of stories to generate')
+parser.add_argument('--verbose', action='store_true', help='verbose')
+
+
+def get_llm(args):
+    llm = crfmChatLLM(
+        model_name=args.model,
+        temperature=args.temperature,
+        max_tokens=args.max_tokens,
+        num_completions=args.num_completions,
+        request_timeout=180
+    )
+    return llm
+
+def get_human_message(args):
+    noun, verb, adj, context, letter_name, letter_profession = "", "", "", "", random.choice(letters), random.choice(letters)
+
+    with(open(f'{PROMPT_DIR}/reminder_stage_1.txt', 'r')) as f:
+        msg = f.read().strip()
+
+    # random letter for name
+    msg = msg.replace("[name_letter]", letter_name)
+    msg = msg.replace("[profession_letter]", letter_profession)
+    
+
+    # # random noun
+    # with open(f'{WORDS_DIR}/nouns.txt', 'r') as f_noun:
+    #     nouns = ast.literal_eval(f_noun.readline())
+    #     noun = random.choice(nouns)
+    #     msg = msg.replace("[noun]", noun)
+    # # random verb
+    # with open(f'{WORDS_DIR}/verbs.txt', 'r') as f_verb:
+    #     verbs = ast.literal_eval(f_verb.readline())
+    #     verb = random.choice(verbs)
+    #     msg = msg.replace("[verb]", verb)
+    # # random adj
+    # with open(f'{WORDS_DIR}/adj.txt', 'r') as f_adj:
+    #     adjs = ast.literal_eval(f_adj.readline())
+    #     adj = random.choice(adjs)
+    #     msg = msg.replace("[adj]", adj)
+    # # random context
+    # with open(f'{WORDS_DIR}/context.txt', 'r') as f_con:
+    #     contexts = ast.literal_eval(f_con.readline())
+    #     context = random.choice(contexts)
+    #     msg = msg.replace("[context]", context)
+
+    if args.verbose: print(msg)
+    return msg, None
+    return msg, {"noun": noun, "verb": verb, "adj": adj, "context": context}
+
+
+def gen_chat(args):
+    response_template = """Here is the story:
+Context: {context}
+Situation CC: {cc}
+Harm CC: {harm_cc}
+Good CC: {good_cc}
+Action CC: {action_cc}
+Prevention CC: {prevention_cc}
+External Cause CC: {external_cause_cc}
+Situation CoC: {coc}
+Good CoC: {good_coc}
+Harm CoC: {harm_coc}
+Action CoC: {action_coc}
+Prevention CoC: {prevention_coc}
+External Cause CoC: {external_cause_coc}
+"""
+
+    llm = get_llm(args)
+    with(open(f'{PROMPT_DIR}/morality_stage_1.txt', 'r')) as f:
+        instruction_text = f.read()
+
+    system_message = SystemMessage(content=instruction_text)
+    # 2-shots by default
+    human_message_0 = HumanMessage(content='Generate a story')
+    s, settings = get_human_message(args)
+    new_message = s.split("Story (the")
+    human_message_1 = HumanMessage(content=new_message[0])
+    
+    examples = []
+    template_var = ["context", "cc", "harm_cc", "good_cc", "action_cc", "prevention_cc", "external_cause_cc", "coc", "good_coc", "harm_coc", "action_coc", "prevention_coc", "external_cause_coc"]
+    csv_file = f'{DATA_DIR}/{CSV_NAME}.csv'
+
+    prompt_tokens_used = 0
+    completion_tokens_used = 0
+
+    # run loop with n stories, increase by num_completions
+    for n_story in tqdm.tqdm(range(0, args.num_stories, args.num_completions)):
+        s, settings = get_human_message(args)
+        new_message = s.split("Story (the")
+        human_message_1 = HumanMessage(content=new_message[0])
+   
+        # read examples from csv file every iteration to add generated samples to the pool of seed examples
+        if args.verbose:
+            print(f"Reading examples from {csv_file} with existing {get_num_items(csv_file)} examples")
+        # read a few examples from the csv file
+        with open(csv_file, 'r') as f:
+            for line in f.readlines():
+                params = line.split(';')
+                example = {k: params[v].strip() for v, k in enumerate(template_var)} 
+                examples.append(example)
+        random.shuffle(examples)
+
+        # 2-shots by default	
+        messages = [system_message]	
+        for i in range(args.num_shots):	
+            messages.append(human_message_0)
+
+            messages.append(AIMessage(content=response_template.format(**examples[i])))	
+        messages.append(human_message_1)	
+        if args.verbose:
+            print(f"------ messages ------")	
+            print(messages)	
+        responses = llm.generate([messages], stop=["System:"])
+        # prompt_tokens_used += responses.llm_output['token_usage']['prompt_tokens']
+        # completion_tokens_used += responses.llm_output['token_usage']['completion_tokens']
+        # price = (prompt_tokens_used * 0.03 + completion_tokens_used * 0.06) / 1000.
+        # update tqdm progress bar with price
+        # tqdm.tqdm.write(f"Price: {price:.2f} USD, Price per story: {price/(n_story+args.num_completions):.2f} USD")
+        for g, generation in enumerate(responses.generations[0]):
+            # print(f"AYESHA LOOK HERE \n: {generation.text}")
+            if args.verbose:
+                print(f"------ Generated Story {n_story+g} ------")
+                print(generation.text)
+                print("------------ Fin --------------")
+            list_var = ["Context", "Situation CC", "Harm CC", "Good CC", "Action CC", "Prevention CC", "External Cause CC", "Situation CoC", "Good CoC", "Harm CoC", "Action CoC", "Prevention CoC", "External Cause CoC"]
+            try:
+                out_vars = get_vars_from_out(generation.text, list_var)
+                # breakpoint()
+            except:
+                print("Error in parsing output")
+                breakpoint()
+            data = [out_vars[k] for k in list_var]
+            story_file = f'{DATA_DIR}/{CSV_NAME}.csv'
+            with open(story_file, 'a') as csvfile:
+                writer = csv.writer(csvfile, delimiter=';')
+                writer.writerow(data)
+        # push to github
+        # push_data(DATA_DIR, REPO_URL)
+    
+    
+if __name__ == "__main__":
+    args = parser.parse_args()
+    # print(f"Generating {args.num_stories} stories")
+    # if args.verbose:
+    #     print(args)
+    gen_chat(args)
